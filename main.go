496c2e55702975bf8da52b11d4d0517f5b39082b
yafu <yafu@yafudeMacBook-Air.local>
2025-07-18 10:35:25 +0800

    test2

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 import(
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
 func main(){
-	fmt.Println("hello")
+	mux :=http.NewServeMux()
+	mux.HandleFunc("/",home)
+
+	log.Print("starting server on :4000")
+
+	err :=http.ListenAndServe(":http",mux)
+	log.Fatal(err)
 }
